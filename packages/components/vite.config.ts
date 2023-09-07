8271b7d0e98c04565ab3c5a7a1123c469ec983ff
equalxp <2106704492@qq.com>
2023-09-07 12:50:38 +0800

    build: none

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -53,14 +53,18 @@ export default defineConfig({
       ]
     },
     lib: {
-      entry: './index.ts'
+      entry: './index.ts',
+      name: "lxxp",
     }
   },
   plugins: [
     vue(),
     dts({
       entryRoot: './src',
-      outputDir: ['../xpxpui/es/src', '../xpxpui/lib/src'],
+      outputDir: [
+        '../xpxpui/es/src',
+        '../xpxpui/lib/src'
+      ],
       //指定使用的tsconfig.json为我们整个项目根目录下,如果不配置,你也可以在components下新建tsconfig.json
       tsConfigFilePath: '../../tsconfig.json'
     }),
